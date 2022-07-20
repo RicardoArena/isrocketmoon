59066f40511a70efb37cc3e88000fd2af1b34c32
Anderson Fae <andersonfae.owl@gmail.com>
2022-07-20 17:36:25 -0300

    User Model remodeled

--- models/User.model.js
+++ models/User.model.js
@@ -2,6 +2,7 @@ const { Schema, model, default: mongoose } = require("mongoose");
 
 const userSchema = new Schema({
   name: { type: String, required: true, trim: true },
+  nickname: { type: String, required: true, trim: true },
   email: {
     type: String,
     required: true,
@@ -9,11 +10,17 @@ const userSchema = new Schema({
     trim: true,
     match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
   },
+  description: { type: String },
+  location: {
+    type: String,
+    enum: ["North America", "South America", "Europe", "Oceania", "Asia"],
+  },
   passwordHash: { type: String, required: true },
   img: { type: String },
   role: { type: String, enum: ["ADMIN", "USER"], default: "USER" },
   isActive: { type: Boolean, default: true },
   disabledOn: { type: Date },
+  typeOfUser: { type: String, enum: ["Pilot", "Owner"], required: true },
 });
 
 const UserModel = model("User", userSchema);
