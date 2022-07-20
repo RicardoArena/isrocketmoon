b0db715edde036f1b23863d92131148d41d35af3
Ricardo Arena <arena_717@hotmail.com>
2022-07-20 18:01:09 -0300

    job model done

---
+++ models/Jobs.model.js
@@ -0,0 +1,41 @@
+const { Schema, model, default: mongoose } = require("mongoose");
+
+const JobsSchema = new Schema({
+  ownership: { type: String, required: true, trim: true },
+  title: { type: String, required: true, trim: true, maxLength: 32 },
+  description: { type: String, required: true, maxLength: 256 },
+  amount: { type: Number, required: true },
+  currency: {
+    type: String,
+    enum: [
+      "Dolar",
+      "Euro",
+      "Peso",
+      "Libra",
+      "Real",
+      "Dolar Canadense",
+      "Iene",
+      "Yuan",
+      "Rublo",
+      "Rupia",
+    ],
+    required: true,
+  },
+  requester: { type: String, required: true },
+  game: {
+    type: String,
+    enum: ["League of Legends", "Counter Strike", "Dota 2", "Tibia"],
+    default: "League of Legends",
+  },
+  publishedDate: { type: Date, default: Date.now() },
+  updateDate: { type: Date, default: Date.now() },
+  status: {
+    type: String,
+    enum: ["Launching", "Processing", "Landed"],
+    default: "Launching",
+  },
+});
+
+const JobsModel = model("Jobs", JobsSchema);
+
+module.exports = UserModel;
