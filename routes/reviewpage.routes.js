e856af67347ebff3f1eca2884fc15f030ca03139
“felipebiermanngit config --global user.name “felipebiermann <biermannfelipe@gmail.com>
2022-07-23 18:07:00 -0300

    CRUD REVIEW PAGE IS DONE.

--- routes/reviewpage.routes.js
+++ routes/reviewpage.routes.js
@@ -54,4 +54,93 @@ router.get("/my-review-page", isAuth, attachCurrentUser, async (req, res) => {
   }
 });
 
+// READ DETAIL
+router.get("/:reviewId", isAuth, attachCurrentUser, async (req, res) => {
+  try {
+    // const loggedInUser = req.currentUser;
+
+    const { reviewId } = req.params;
+
+    const foundReview = await ReviewModel.findOne({ _id: reviewId });
+
+    return res.status(200).json(foundReview);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+
+// EDIT REVIEW PAGE
+router.patch("/edit/:reviewId", isAuth, attachCurrentUser, async (req, res) => {
+  try {
+    const loggedInUser = req.currentUser;
+
+    const { reviewId } = req.params;
+
+    const body = { ...req.body };
+
+    // const jobs = await JobsModel.findOne({ _id: jobsId });
+
+    // if (jobs.owner !== loggedInUser._id) {
+    //   return res
+    //     .status(401)
+    //     .json({ message: "Você não pode alterar esse job!" });
+    // }
+
+    const updateReviewPage = await ReviewModel.findOneAndUpdate(
+      { _id: reviewId },
+      { ...body },
+      { new: true, runValidators: true }
+    );
+
+    return res.status(200).json(updateReviewPage);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+
+// DELETE REVIEW PAGE
+
+router.delete(
+  "/delete/:reviewId",
+  isAuth,
+  attachCurrentUser,
+  async (req, res) => {
+    try {
+      const { reviewId } = req.params;
+      const loggedInUser = req.currentUser;
+
+      const reviewPage = await ReviewModel.findOne({ _id: reviewId });
+
+      // if (job.owner !== loggedInUser._id) {
+      //   return res
+      //     .status(401)
+      //     .json({ message: "Você não pode deletar esse album." });
+      // }
+
+      const deletedReviewPage = await ReviewModel.deleteOne({
+        _id: req.params.reviewId,
+      });
+
+      // await ReviewModel.updateMany(
+      //   { jobs: jobsId },
+      //   { $pull: { jobs: jobsId } }
+      // );
+
+      // await UserModel.findOneAndUpdate(
+      //   { _id: loggedInUser._id },
+      //   { $pull: { jobs: jobsId } },
+      //   { runValidators: true }
+      // );
+
+      return res.status(200).json(reviewPage);
+    } catch (err) {
+      console.log(err);
+
+      return res.status(500).json(err);
+    }
+  }
+);
+
 module.exports = router;
